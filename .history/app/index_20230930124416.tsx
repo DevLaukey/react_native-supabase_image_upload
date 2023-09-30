b6b3d35b64436888d76387f4fa6f86b71f0f0d73
DevLaukey <63729244+DevLaukey@users.noreply.github.com>
2023-09-30 12:47:23 +0300

    Setting up login

---
+++ .history/app/index_20230930124416.tsx
@@ -0,0 +1,111 @@
+import {
+  Alert,
+  View,
+  Button,
+  TextInput,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import React from "react";
+import Spinner from "react-native-loading-spinner-overlay";
+import { supabaseClient } from "../config/initSupabase";
+
+const Login = () => {
+  const [email, setEmail] = useState("laukeymwaura@gmail.com");
+  const [password, setPassword] = useState("password");
+  const [loading, setLoading] = useState(false);
+
+  // Sign in with email and password
+  const onSignInPress = async () => {
+    setLoading(true);
+
+    const { error } = await supabaseClient.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) Alert.alert(error.message);
+    setLoading(false);
+  };
+
+  // Create a new user
+  const onSignUpPress = async () => {
+    setLoading(true);
+    const { error } = await supabaseClient.auth.signUp({
+      email: email,
+      password: password,
+    });
+
+      if (error) Alert.alert(error.message);
+      else Alert.alert("Sign up successful");
+    setLoading(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Spinner visible={loading} />
+
+      <Text style={styles.header}>My Cloud</Text>
+
+      <TextInput
+        autoCapitalize="none"
+        placeholder="john@doe.com"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.inputField}
+      />
+      <TextInput
+        placeholder="password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.inputField}
+      />
+
+      <TouchableOpacity onPress={onSignInPress} style={styles.button}>
+        <Text style={{ color: "#fff" }}>Sign in</Text>
+      </TouchableOpacity>
+      <Button
+        onPress={onSignUpPress}
+        title="Create Account"
+        // color={"#fff"}
+      ></Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 200,
+    padding: 20,
+    backgroundColor: "#151515",
+  },
+  header: {
+    fontSize: 30,
+    textAlign: "center",
+    margin: 50,
+    color: "#fff",
+  },
+  inputField: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#2b825b",
+    borderRadius: 4,
+    padding: 10,
+    color: "#fff",
+    backgroundColor: "#363636",
+  },
+  button: {
+    marginVertical: 15,
+    alignItems: "center",
+    backgroundColor: "#2b825b",
+    padding: 12,
+    borderRadius: 4,
+  },
+});
+
+export default Login;
